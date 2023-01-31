77391763c125a15f8137304079ff5f5cee0699d5
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-01-31 05:28:49 -0300

    Add more packages

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -101,17 +101,30 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # HW-probing
+    inxi
+    pciutils # Provides lspci
+
     # Text editor
     helix
 
     # Image editing
-    gimp
     gmic
+    gmic-qt
+    (gimp-with-plugins.override { plugins = with gimpPlugins; [ gmic ]; })
+    rawtherapee
 
     # Git-related
     gh
     git
     sublime-merge
+
+    # Password management
+    keepassxc
+
+    # C++ stuff
+    clang
+    qtcreator
     
     (vscode-with-extensions.override {
          vscodeExtensions = with vscode-extensions; [
@@ -121,6 +134,7 @@
          ];
     })
 
+    # X11 stuff
     xclip
   ];
 
@@ -150,13 +164,5 @@
     ];
   };
 
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  system.stateVersion = "22.11";
 }
