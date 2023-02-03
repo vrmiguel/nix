48cdbb571b8c96114989eeb11f0eb64c1d14102e
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-02-03 01:18:21 -0300

    Use `iwd` instead of `wpa_supplicant`

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -25,7 +25,16 @@ in
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  networking.networkmanager.enable = true;
+  networking = {
+    wireless.iwd.enable = true;
+    
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+  };
+  
+ 
 
   # Locale configs
   time.timeZone = "America/Sao_Paulo";
